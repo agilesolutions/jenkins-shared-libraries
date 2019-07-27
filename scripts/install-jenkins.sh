bb3f03c0b80194d8119576c8ccfd7cbf59942a8c
Robert Rong <robert.rong@agile-solutions.ch>
2019-07-27 11:29:45 +0200

    fix

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -2,6 +2,4 @@
 Helm install stable/jenkins \
     --name jenkins \
     --namespace jenkins \
-    --values ../jenkins/jenkins-values.yml \
-    --set Master.HostName=localhost
- 
+    --values ../jenkins/jenkins-values.yml
